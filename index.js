167e08450fc128047a5b7ca6ae041ffad1871b48
Michael Douglas <michaellawrencedouglas@gmail.com>
2017-06-20 06:57:35 -0400

    added functionality for adding videos

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var app={
+	searchTerm:'',
+	nextPageToken:''
+};
+
 var youTubeURL='https://www.googleapis.com/youtube/v3/search';
 function queryAPI(searchTerm){
 	var query={
@@ -7,8 +12,31 @@ function queryAPI(searchTerm){
 	};
 	$.getJSON(youTubeURL,query,devInfo);
 }
+function queryAPI4More(searchTerm){
+	var query={
+		q:searchTerm + ' in:title',
+		part:'snippet',
+		key:'AIzaSyCR9HOggl42tEUr97BuhC_DcpWOaPn8ano',
+		pageToken:app.nextPageToken
+	};
+	$.getJSON(youTubeURL,query,devInfo);
+}
 function devInfo(data){
 	$('#output').empty();
+	app.nextPageToken=data.nextPageToken;
+	data.items.map(function(item, index){
+		var imgURL=item.snippet.thumbnails.default.url;
+		var newElem=$('#template').clone();
+		newElem.find('img').attr('src',imgURL);
+		var vidURL=newElem.find('a').attr('href')+item.id.videoId;
+		newElem.find('a').attr('href',vidURL);
+		newElem.removeAttr('hidden');
+		$('#output').append(newElem);
+	});
+}
+function devInfo2(data){
+	$('#output').empty();
+	app.nextPageToken=data.nextPageToken;
 	data.items.map(function(item, index){
 		var imgURL=item.snippet.thumbnails.default.url;
 		var newElem=$('#template').clone();
@@ -23,10 +51,22 @@ function devInfo(data){
 function onSubmit(){
 	$('#youTubeSearch').on('submit',function(event){
 		event.preventDefault();
-		queryAPI($('#vidSearch').val());
+		var searchTerm=$('#vidSearch').val();
+		app.searchTerm=searchTerm;
+		queryAPI(searchTerm);
+		$('#btnShowMore').removeAttr('hidden');
 	});
+}
+
+function onShowMore(){
+	$('#frmShowMore').on('submit',function(event){
+		event.preventDefault();
+		queryAPI4More(app.searchTerm);
+	});
+
 }
 
 $(function(){
 	onSubmit();
+	onShowMore();
 });
